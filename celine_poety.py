bc7fc0464b920907e50d24c266180428b439b16e
Celine Kim <celinekim@Celines-MacBook-Pro.local>
2020-04-21 16:45:36 -0700

    Hallloooo my pOEm

---
+++ celine_poety.py
@@ -0,0 +1,34 @@
+'''
+    will it rain today
+'''
+import yeah
+import nosun 
+
+PUPPY_JOY = MAX_INT
+
+def walk_my_dog_in_rain(get_over_it):
+    get_over_it += 10*PUPPY_JOY
+
+def some_sun_for_me(hope):
+    maybe = 0.9
+    yes = 0.8
+    no = 0.0
+    get_over_it = 0
+    if yes < no:
+        print('yeah it rains everyday')
+    while(1):
+        rain+= rain
+        if (sys.stdin == True):
+            walk_my_dog_in_rain(get_over_it)
+            return hope + 1
+        else:
+            cloud+= cloud
+
+hope = 0
+some_sun_for_me(hope)
+
+'''
+It has been raining everyday where I am right now. 
+I cannot really go out as much and there is NO SUN.
+It's only a small issue, though, and I am sure there will be sun tomorrow :>
+'''
